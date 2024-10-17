6a9a145fcf5a783a12ef3cc4fca346a6389ed398
davign <183004594+Davi-GN@users.noreply.github.com>
2024-10-17 12:34:28 -0300

    Implementação de Tarefas

--- frontend/src/pages/ToDoList/index.js
+++ frontend/src/pages/ToDoList/index.js
@@ -9,34 +9,38 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import { Typography } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
+    height: "100vh",
+    backgroundColor: theme.palette.background.main,
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
-    margin: '2rem'
+    gap: theme.spacing(4),
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(6),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(6),
   },
   inputContainer: {
     display: 'flex',
     width: '100%',
-    marginBottom: '1rem'
   },
   input: {
     flexGrow: 1,
-    marginRight: '1rem'
+    marginRight: theme.spacing(2),
   },
   listContainer: {
     width: '100%',
-    height: '100%',
-    marginTop: '1rem',
-    backgroundColor: '#f5f5f5',
-    borderRadius: '5px',
+    marginTop: theme.spacing(2),
   },
   list: {
-    marginBottom: '5px'
+    backgroundColor: theme.palette.light.main,
+    marginBottom: theme.spacing(1),
+    ...theme.shape
   }
-});
+}));
 
 const ToDoList = () => {
   const classes = useStyles();
@@ -94,34 +98,42 @@ const ToDoList = () => {
 
   return (
     <div className={classes.root}>
-      <div className={classes.inputContainer}>
-        <TextField
-          className={classes.input}
-          label="Nova tarefa"
-          value={task}
-          onChange={handleTaskChange}
-          variant="outlined"
-        />
-        <Button variant="contained" color="primary" onClick={handleAddTask}>
-          {editIndex >= 0 ? 'Salvar' : 'Adicionar'}
-        </Button>
-      </div>
-      <div className={classes.listContainer}>
-        <List>
-          {tasks.map((task, index) => (
-            <ListItem key={index} className={classes.list}>
-              <ListItemText primary={task.text} secondary={task.updatedAt.toLocaleString()} />
-              <ListItemSecondaryAction>
-                <IconButton onClick={() => handleEditTask(index)}>
-                  <EditIcon />
-                </IconButton>
-                <IconButton onClick={() => handleDeleteTask(index)}>
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
+      <Typography
+        variant='h4'
+        color="primary"
+      >
+        Tarefas
+      </Typography>
+      <div>
+        <div className={classes.inputContainer}>
+          <TextField
+            className={classes.input}
+            label="Nova tarefa"
+            value={task}
+            onChange={handleTaskChange}
+            variant="outlined"
+          />
+          <Button variant="contained" color="primary" onClick={handleAddTask}>
+            {editIndex >= 0 ? 'Salvar' : 'Adicionar'}
+          </Button>
+        </div>
+        <div className={classes.listContainer}>
+          <List>
+            {tasks.map((task, index) => (
+              <ListItem key={index} className={classes.list}>
+                <ListItemText primary={task.text} secondary={task.updatedAt.toLocaleString()} />
+                <ListItemSecondaryAction>
+                  <IconButton onClick={() => handleEditTask(index)}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => handleDeleteTask(index)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
     </div>
   );
