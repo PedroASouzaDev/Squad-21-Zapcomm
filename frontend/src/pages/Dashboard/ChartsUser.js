73c1b79c3d5cd852295f272c49110bfc4e3d1c95
davign <183004594+Davi-GN@users.noreply.github.com>
2024-10-21 09:30:38 -0300

    Dashboard - Ajuste de gap do gráfico

--- frontend/src/pages/Dashboard/ChartsUser.js
+++ frontend/src/pages/Dashboard/ChartsUser.js
@@ -14,7 +14,7 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import brLocale from 'date-fns/locale/pt-BR';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { Button, Stack, TextField } from '@mui/material';
-import Typography from "@material-ui/core/Typography";
+import Box from '@mui/material/Box';
 import api from '../../services/api';
 import { format } from 'date-fns';
 import { toast } from 'react-toastify';
@@ -106,19 +106,16 @@ export const ChatsUser = () => {
     }
 
     return (
-        <>
+        <Box>
             <Stack direction={'row'} spacing={2} alignItems={'center'} justifyContent={'right'} sx={{ my: 2, }} >
-
                 <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={brLocale}>
                     <DatePicker
                         value={initialDate}
                         onChange={(newValue) => { setInitialDate(newValue) }}
                         label="Inicio"
                         renderInput={(params) => <TextField fullWidth {...params} sx={{ width: '20ch' }} />}
-
                     />
                 </LocalizationProvider>
-
                 <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={brLocale}>
                     <DatePicker
                         value={finalDate}
@@ -127,11 +124,9 @@ export const ChatsUser = () => {
                         renderInput={(params) => <TextField fullWidth {...params} sx={{ width: '20ch' }} />}
                     />
                 </LocalizationProvider>
-
                 <Button className="buttonHover" onClick={handleGetTicketsInformation} variant='contained'>Filtrar</Button>
-
             </Stack>
             <Bar options={options} data={dataCharts} style={{ maxWidth: '100%', maxHeight: '280px', }} />
-        </>
+        </Box>
     );
 }
